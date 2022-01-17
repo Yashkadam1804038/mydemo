9d9304a033c6445fd3f14f2aaef92a1fe0c83491
Yashkadam1804038 <yashkadam1504@gmail.com>
2022-01-17 17:02:00 +0530

    SQL files

---
+++ CREATE DATABASE customers.sql
@@ -0,0 +1,21 @@
+--CREATE DATABASE customers
+
+/*CREATE TABLE cust(
+   fname VARCHAR(50),
+   lname VARCHAR(50),
+   pkg_nm INT,
+   speed VARCHAR(50)
+
+);*/
+
+--select * FROM cust
+
+/*INSERT INTO cust ( fname, lname, pkg_nm, speed)
+ VALUES
+  ( 'abc', 'xyz', 21, '82 mb/sec'),
+   ( 'yer', 'pql', 22, '94 mb/sec'),
+    ( 'rte', 'oij', 23, '54 mb/sec'),
+     ( 'eh', 'ef', 24, '77 mb/sec'),
+      ( 'er', 'aam', 25, '65 mb/sec'),
+      ( 'ff', 'tt', 26, '12 mb/sec'),
+      ( 'gg', 'kk', 27, '50 mb/sec') */
